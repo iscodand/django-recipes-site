02ecf2c0bd9c644e2b2d10f6f55e69738dda0ed7
iscodand <iscodand@outlook.com>
2022-08-09 14:40:15 -0300

    Editing Django Admin filters

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Recipes
 
 
-admin.site.register(Recipes)
+class ListingRecipes(admin.ModelAdmin):
+    list_display = ('id', 'recipe_name', 'category')
+    list_display_links = ('id', 'recipe_name')
+    search_fields = ('recipe_name', 'id')
+    list_filter = ('category',)
+
+
+admin.site.register(Recipes, ListingRecipes)
