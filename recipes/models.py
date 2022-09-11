029f1cf1ba8aa1492cd9273b0f68ecc87dc45e83
iscodand <iscodand@outlook.com>
2022-09-11 18:14:12 -0300

    Add and Update: Update models with new max_length / Make Migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from datetime import datetime
-from persons.models import Person
+from django.contrib.auth.models import User
 
 
 class Recipes(models.Model):
-    person_name = models.ForeignKey(Person, on_delete=models.CASCADE)
+    person_name = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe_name = models.CharField(max_length=45)
     duration = models.IntegerField()
-    rendiment = models.IntegerField()
+    rendiment = models.CharField(max_length=25)
     category = models.CharField(max_length=25)
-    description = models.TextField(max_length=255)
-    ingredients = models.TextField(max_length=255)
+    description = models.TextField(max_length=528)
+    ingredients = models.TextField(max_length=528)
     datetime = models.DateTimeField(default=datetime.now, blank=False)
     image = models.ImageField(upload_to='images/%d/%m/%Y/', blank=True)
     publicated = models.BooleanField(default=False)
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from recipes.models import Recipes
 
 
