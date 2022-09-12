f46b9227bee60a818ff6fec4d4cd49a1774e0ae9
iscodand <iscodand@outlook.com>
2022-09-12 16:40:21 -0300

    Add: Messages

--- alura_recipes/settings_template.py
+++ alura_recipes/settings_template.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 
@@ -125,3 +126,10 @@ STATIC_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Message Tags
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
